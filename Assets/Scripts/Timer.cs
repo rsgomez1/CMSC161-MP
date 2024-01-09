5c4b41e0b76398fff95f53a9539f29cc7f396621
Ferrand Chester Noche <fdnoche@up.edu.ph>
2024-01-09 11:38:41 +0800

    Stop Time during Load

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -2,27 +2,50 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-using TMPro;
-using UnityEngine.SceneManagement;
 
 public class Timer : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI timerText;
-    float elapsedTime;
+    float startTime;
+    bool timerStopped = false;
 
-    private void Awake()
+    private void Start()
     {
-        elapsedTime = 0f;
-        Debug.Log("PTANGENA");
+        startTime = Time.time;
+        Debug.Log("Timer started");
     }
 
     // Update is called once per frame
     void Update()
     {
-        elapsedTime += Time.deltaTime;
+        if (!timerStopped)
+        {
+            float elapsedTime = Time.time - startTime;
 
-        int minutes = Mathf.FloorToInt(elapsedTime / 60);
-        int seconds = Mathf.FloorToInt(elapsedTime % 60);
-        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+            int minutes = Mathf.FloorToInt(elapsedTime / 60);
+            int seconds = Mathf.FloorToInt(elapsedTime % 60);
+            timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && other.CompareTag("Collider"))
+        {
+            timerStopped = true;
+            float elapsed = Time.time - startTime;
+            Debug.Log("Collision with Player and Collider detected. Elapsed time: " + elapsed);
+            // Use the elapsed time for any specific action you need
+        }
+        else if (other.CompareTag("Player"))
+        {
+            float elapsed = Time.time - startTime;
+            Debug.Log("Collision with Player detected. Elapsed time: " + elapsed);
+        }
+        else if (other.CompareTag("Collider"))
+        {
+            float elapsed = Time.time - startTime;
+            Debug.Log("Collision with Collider detected. Elapsed time: " + elapsed);
+        }
     }
 }
